b9981875f1d25fe5a6565606d266354494a96d84
Simon <sima@chetverukhin.ru>
2017-06-05 19:46:30 +0300

    main-page-desktop

--- src/scripts/logic.js
+++ src/scripts/logic.js
@@ -1,3 +1,5 @@
+// INPUTS
+
 $( ".input-text" ).focus(function() {
 	$( this ).next( ".input__desc" ).addClass( "input__desc--active" );
 });
@@ -7,4 +9,22 @@ $( ".input-text" ).blur(function() {
 	if (inputText.length == 0) {
 		$( this ).next( ".input__desc" ).removeClass( "input__desc--active" );
 	}
+});
+
+// TABS
+
+$(".tab-templates").click(function () {
+	$(".payments-lists--templates").show();
+	$(".payments-lists--repeat").hide();
+	$(this).addClass("tab--active");
+	$(".tab-repeat").removeClass("tab--active");
+	$(".tabs__current").addClass("tabs__current--2");
+});
+
+$(".tab-repeat").click(function () {
+	$(".payments-lists--repeat").show();
+	$(".payments-lists--templates").hide();
+	$(this).addClass("tab--active");
+	$(".tab-templates").removeClass("tab--active");
+	$(".tabs__current").removeClass("tabs__current--2");
 });

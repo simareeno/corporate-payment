e162653a070bbb4242a1413f35c2b727a31cf288
Simon <sima@chetverukhin.ru>
2017-06-16 16:31:03 +0300

    more on suggest

--- src/scripts/logic.js
+++ src/scripts/logic.js
@@ -26,6 +26,77 @@ $(".input--sms").keyup(function () {
 	}
 });
 
+// Suggest
+
+let iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+let suggestTop = document.querySelector(".mobile .suggest-top");
+let suggestBottom = document.querySelector(".mobile .suggest-bottom");
+if (suggestBottom) {
+	suggestBottom.style.transform = "translateY(" + screenHeight + "px)";
+}
+
+
+function showSuggest(input) {
+	let bottomFromTop = 120;
+	let topFromTop = 0;
+	let pxToTop = input.offset().top;
+	let top = $(window).scrollTop();
+
+	if (iOS) {
+		bottomFromTop = -top + 300;
+		topFromTop = -top + 200;
+	} else {
+		$("body").stop().animate({scrollTop:pxToTop - 70}, 200, "swing");
+	}
+
+	suggestTop.style.transform = "translateY(" + topFromTop + "px)";
+	suggestTop.style.visibility = "visible";
+	suggestTop.style.height = "44px";
+	suggestTop.style.padding = "12px";
+
+	suggestBottom.style.transform = "translateY(" + bottomFromTop + "px)";
+	suggestBottom.style.visibility = "visible";
+	suggestBottom.style.height = "100%";
+	suggestBottom.style.padding = "12px";
+}
+
+function hideSuggest(input) {
+	let pxToTop = input.offset().top;
+	suggestTop.style.transform = "translateY(-" + pxToTop + "px)";
+	suggestBottom.style.transform = "translateY(" + screenHeight + "px)";
+
+	setTimeout(function () {
+		suggestTop.style.visibility = "hidden";
+		suggestTop.style.height = "0";
+		suggestTop.style.padding = "0";
+
+		suggestBottom.style.visibility = "hidden";
+		suggestBottom.style.height = "0";
+		suggestBottom.style.padding = "0";
+	}
+	, 200);
+}
+
+$(".input-receiver .input-text").focus(function () {
+	showSuggest($(this));
+});
+
+$(".input-receiver .input-text").blur(function () {
+	let thisInput = $(this);
+	hideSuggest(thisInput);
+});
+
+$(".suggest-top__close").click(function () {
+	hideSuggest($(this));
+});
+
+$(".suggest-single").mousedown(function () {
+	let suggestTitle = $(this).find(".suggest-single__title").text();
+	$(".input-receiver .input-text").val(suggestTitle);
+	$(".input-receiver .input__desc").addClass( "input__desc--active" );
+	hideSuggest($(this));
+});
+
 
 // Кнопки в форме
 
